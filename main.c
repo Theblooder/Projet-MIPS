df0917c418f7f6da3f6e22a83eb028befa117060
Theblooder <remi.gilger@orange.fr>
2021-11-24 10:59:21 +0100

    Ajout lecture instructions dans fichiers

--- main.c
+++ main.c
@@ -5,57 +5,56 @@
 
 int main(int argc, char * argv[])
 {
-	//	FILE *fic;
-
-	// char filename[32];
-	// char* PathTests = "tests/";
+	FILE *fic;
+	char filename[32] = "tests/in1.txt";
+	// char *PathTests = "tests/";
 	// strcpy(filename, PathTests);
 	// strcat(filename, argv[1]);
 
-	// fic = fopen(filename, "r");
-	// if(fic == NULL) {
-	// 	perror("Probleme ouverture du fichier");
-	// 	exit(0);
-	// }
-	
-	// char instruction[20];
-
-	// while(!feof(fic)) {
-	// 	fscanf(fic, "%s", instruction);
-	// 	printf("Valeur lue = %s\n", instruction);
-	// }
-
-	// fclose(fic);
-
-
-	char instruction[32] = "ADD $3 $2 $3";
-	//char instruction[32] = "ADDI $3 $2 32700";
-	int binaireInstruction[32] = {0};
-	int hexadecimalInstruction[8]= {0};
-	char operation[5];
-	int values[3];
-
-	instructionValue(instruction, values, operation);
-	printf("%s\n", operation);
-	printf("%d\n", values[0]);
-	printf("%d\n", values[1]);
-	printf("%d\n", values[2]);
-	
-	//printf("%ld\n", strlen(operation));	//Erreur
-
-	createBinaryInstruction(operation, values, binaireInstruction);
-
-	convertBinaireIntoHex(binaireInstruction, hexadecimalInstruction);
-	
-	displayBinary(binaireInstruction);
-
-	displayHexadecimal(hexadecimalInstruction);
+	fic = fopen(filename, "r");
+	if(fic == NULL) {
+		perror("Probleme ouverture du fichier");
+		exit(0);
+	}
 
 	
+	char charInstruction;
+	char instruction[32];
 
-	//convertIntoBinaireADD(instruction, binaireInstruction);
+	int cmptChar = 0;
 
-	//printf("%s\n", binaireInstruction);
+	while(!feof(fic)) {
+		fscanf(fic, "%c", &charInstruction);
+		if(charInstruction == '\n') {
+			instruction[cmptChar] = '\0';
+			cmptChar = 0;
+
+			int binaireInstruction[32] = {0};
+			int hexadecimalInstruction[8]= {0};
+			char operation[5];
+			int values[3];
+
+			instructionValue(instruction, values, operation);
+			printf("%s\n", operation);
+			printf("%d\n", values[0]);
+			printf("%d\n", values[1]);
+			printf("%d\n", values[2]);
+
+			createBinaryInstruction(operation, values, binaireInstruction);
+
+			convertBinaireIntoHex(binaireInstruction, hexadecimalInstruction);
+	
+			displayBinary(binaireInstruction);
+
+			displayHexadecimal(hexadecimalInstruction);
+		}
+		else {
+			instruction[cmptChar++] = charInstruction;
+		}
+
+	}
+
+	fclose(fic);
 
 	return 0;
 }
